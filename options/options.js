6399028f8dc2f94e8d4fca308a0667cba5b2687a
Crowdbotics <team@crowdbotics.com>
2024-03-08 21:16:58 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "little-snow-46893",
+  url: "https://little-snow-46893.botics.co",
+  api: "https://little-snow-46893.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
